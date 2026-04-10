c7c7c57897f7cf1ae123149392ba367f3ba36420
D1ffic00lt <dm.filinov@gmail.com>
2026-04-11 00:12:41 +0300

    pep8 lie fix

--- saltai/artifacts/store/base.py
+++ saltai/artifacts/store/base.py
@@ -7,17 +7,21 @@ from saltai.utils.typing.json_types import JSONObject, PathLike
 
 
 class BaseArtifactStore(ArtifactStore):
-    def put(self, local_path: PathLike, *, kind: str, name: str, meta: JSONObject | None = None) -> ArtifactRef:
-        # ignore: Type of 'put' is incompatible with 'ArtifactStore'
+    def put(  # type: ignore
+            self,
+            local_path: PathLike,
+            *,
+            kind: str,
+            name: str,
+            meta: JSONObject | None = None
+    ) -> ArtifactRef:
         pass
 
-    def get(self, ref: ArtifactRef, *, dst_dir: PathLike) -> PathLike:
-        # ignore: Type of 'put' is incompatible with 'ArtifactStore'
+    def get(self, ref: ArtifactRef, *, dst_dir: PathLike) -> PathLike:  # type: ignore
         pass
 
     def exists(self, ref: ArtifactRef) -> bool:
         raise NotImplementedError
 
-    def list(self, *, kind: str | None = None) -> Sequence[ArtifactRef]:
-        # ignore: Type of 'put' is incompatible with 'ArtifactStore'
+    def list(self, *, kind: str | None = None) -> Sequence[ArtifactRef]:  # type: ignore
         pass
