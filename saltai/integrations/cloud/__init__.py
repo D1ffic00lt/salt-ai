17dd0c6665574ebb281dd671c3c087aa6b65945a
D1ffic00lt <dm.filinov@gmail.com>
2026-04-22 20:34:22 +0300

    added more smoke examples and atrifact store

--- saltai/integrations/cloud/__init__.py
+++ saltai/integrations/cloud/__init__.py
@@ -1,15 +1,20 @@
 from __future__ import annotations
 
+from saltai.integrations.cloud.artifact_store import CloudArtifactStore, CloudArtifactStoreError
 from saltai.integrations.cloud.client import CloudApiError, CloudClient, CloudClientError
+from saltai.integrations.cloud.helpers import create_cloud_event_bus
 from saltai.integrations.cloud.logger import CloudLogger, CloudLoggerError
 from saltai.integrations.cloud.run_logger import CloudRunLogger, CloudRunLoggerError
 
 __all__ = (
-    "CloudLogger",
-    "CloudLoggerError",
+    "CloudApiError",
+    "CloudArtifactStore",
+    "CloudArtifactStoreError",
     "CloudClient",
     "CloudClientError",
-    "CloudApiError",
+    "CloudLogger",
+    "CloudLoggerError",
     "CloudRunLogger",
     "CloudRunLoggerError",
+    "create_cloud_event_bus",
 )
