477c7228264f8228c48085a6d5a4462aed92603a
D1ffic00lt <dm.filinov@gmail.com>
2026-04-22 20:56:58 +0300

    test fix

--- saltai/integrations/cloud/__init__.py
+++ saltai/integrations/cloud/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from saltai.integrations.cloud.artifact_store import CloudArtifactStore, CloudArtifactStoreError
 from saltai.integrations.cloud.client import CloudApiError, CloudClient, CloudClientError
-from saltai.integrations.cloud.helpers import create_cloud_event_bus
+from saltai.integrations.cloud.helpers import create_cloud_event_bus, create_cloud_run_context
 from saltai.integrations.cloud.logger import CloudLogger, CloudLoggerError
 from saltai.integrations.cloud.run_logger import CloudRunLogger, CloudRunLoggerError
 
@@ -17,4 +17,5 @@ __all__ = (
     "CloudRunLogger",
     "CloudRunLoggerError",
     "create_cloud_event_bus",
+    "create_cloud_run_context",
 )
